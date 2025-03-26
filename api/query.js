9e7b903e4886cf6df1397bc80efc1bb99846b120
faust6312 <153928430+faust6312@users.noreply.github.com>
2025-03-26 02:21:12 -0500

    Update query.js

--- api/query.js
+++ api/query.js
@@ -46,9 +46,9 @@ export default async function handler(req, res) {
       }
     };
 
-    res.status(200).json(result);
+    return res.status(200).json(result);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    return res.status(500).json({ error: error.message });
   }
 }
 
